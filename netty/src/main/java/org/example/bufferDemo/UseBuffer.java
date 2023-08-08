23259501e8bbbf052bf433ccd4939069d0d3336d
bin <bin@>
2023-08-08 23:02:41 +0800

    测试

--- netty/src/main/java/org/example/bufferDemo/UseBuffer.java
+++ netty/src/main/java/org/example/bufferDemo/UseBuffer.java
@@ -10,7 +10,52 @@ public class UseBuffer {
 
     public static void allocatTest(){
         intBuffer = IntBuffer.allocate(20);
+        log.info("position = "+ intBuffer.position());
+        log.info("limit = "+ intBuffer.limit());
+        log.info("capacity = "+ intBuffer.capacity());
+    }
+    public static void putTest(){
+        for (int i = 0; i < 5; i++) {
+            intBuffer.put(i);
+        }
+        log.info("-------------after put--------------");
+        log.info("position = "+ intBuffer.position());
+        log.info("limit = "+ intBuffer.limit());
+        log.info("capacity = "+ intBuffer.capacity());
+    }
 
+    public static void flipTest(){
+        intBuffer.flip();
+
+        log.info("-------------after flip--------------");
+        log.info("position = "+ intBuffer.position());
+        log.info("limit = "+ intBuffer.limit());
+        log.info("capacity = "+ intBuffer.capacity());
+    }
+
+    public static void getTest(){
+        for (int i = 0; i < 2; i++) {
+            int j = intBuffer.get();
+            log.info("j = " + j);
+        }
+        log.info("-------------after get 2 int--------------");
+        log.info("position = "+ intBuffer.position());
+        log.info("limit = "+ intBuffer.limit());
+        log.info("capacity = "+ intBuffer.capacity());
+        for (int i = 0; i < 3; i++) {
+            int j = intBuffer.get();
+            log.info("j = " + j);
+        }
+        log.info("-------------after get 3 int--------------");
+        log.info("position = "+ intBuffer.position());
+        log.info("limit = "+ intBuffer.limit());
+        log.info("capacity = "+ intBuffer.capacity());
+    }
+    public static void main(String[] args) {
+        allocatTest();
+        putTest();
+        flipTest();
+        getTest();
     }
 
 }
